77a54fa0ca9b2b77d13eab53acda20b4d8f6c379
ajs <adam.simmonds@eduserv.org.uk>
2013-04-08 15:47:10 +0100

    Added an example body sub-layout

---
+++ Source/Website/BritishLibrary.Website/layouts/BritishLibrary/Websites/VoS/Sublayout_BritishLibrary_VoS_Body.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="c#" AutoEventWireup="true" TargetSchema="http://schemas.microsoft.com/intellisense/ie5"  Inherits="Layouts.Sublayout_britishlibrary_vos_body.Sublayout_britishlibrary_vos_bodySublayout" CodeFile="~/layouts/BritishLibrary/Websites/VoS/Sublayout_BritishLibrary_VoS_Body.ascx.cs" %>
+
+<h1>
+    <sc:Text runat="server" Field="Title" /> - (title field from Sublayout)
+</h1>
---
+++ Source/Website/BritishLibrary.Website/layouts/BritishLibrary/Websites/VoS/Sublayout_BritishLibrary_VoS_Body.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Layouts.Sublayout_britishlibrary_vos_body {
+  
+	/// <summary>
+	/// Summary description for Sublayout_britishlibrary_vos_bodySublayout
+	/// </summary>
+  public partial class Sublayout_britishlibrary_vos_bodySublayout : System.Web.UI.UserControl 
+	{
+    private void Page_Load(object sender, EventArgs e) {
+      // Put user code to initialize the page here
+    }
+  }
+}
