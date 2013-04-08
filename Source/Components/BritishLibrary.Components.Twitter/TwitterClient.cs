4a317b4d41dd0439de5eeecdb8653ec0740c4814
ajs <adam.simmonds@eduserv.org.uk>
2013-04-08 15:00:34 +0100

    Added example Twitter app

---
+++ Source/Components/BritishLibrary.Components.Twitter/TwitterClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace BritishLibrary.Components.Twitter
+{
+    public class TwitterClient
+    {
+        private string _consumerKey = "22NBcg6hb2tYay78f3vFqQ";
+        private string _consumerSecret = "R4I9N1KUHUXgKpfgmowUVXJmDZLgCr77gatwTkcD4o";
+        private string _accessToken = "107821248-JK0flGmKv7MrNVuNilqAzkYizIXya4S0ODdEQ8no";
+        private string _accessTokenSecret = "Yv7BlpBehXitCW3fgcFE4dBKFMoQRWjvkcZnyWNjU0";
+        private TwitterService service;
+
+        public TwitterClient()
+        {
+            service = new TwitterService(_consumerKey, _consumerSecret);
+            service.AuthenticateWith(_accessToken, _accessTokenSecret);
+        }
+
+        public List<KeyValuePair<string, string>> GetTimelineTweets()
+        {
+            var tweets = new List<KeyValuePair<string,string>>();
+
+            var timelineTweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = "adamsimsy" });
+
+            timelineTweets.ToList().ForEach(x => tweets.Add(new KeyValuePair<string,string>(x.Author.ScreenName, x.Text)));
+
+            return tweets;
+        }
+
+        public void Tweet(string status)
+        {
+            service.SendTweet(new SendTweetOptions { Status = status });
+        }
+    }
+}
---
+++ Source/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BritishLibrary.Components.Twitter;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterClient client = new TwitterClient();
+
+            Console.WriteLine(client.GetTimelineTweets().First().Value);
+            var status = Console.ReadLine();
+
+            client.Tweet(status);
+        }
+    }
+}
