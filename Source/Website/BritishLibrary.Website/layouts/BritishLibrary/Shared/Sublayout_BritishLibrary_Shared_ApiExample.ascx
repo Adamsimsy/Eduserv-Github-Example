fcb99332842f134de000600038205130a4ae5490
Adam Simmonds <adamsimsy@gmail.com>
2013-04-09 01:41:02 +0100

    Added API example

---
+++ Source/Website/BritishLibrary.Website/layouts/BritishLibrary/Shared/Sublayout_BritishLibrary_Shared_ApiExample.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="c#" AutoEventWireup="true" TargetSchema="http://schemas.microsoft.com/intellisense/ie5"  Inherits="Layouts.Sublayout_britishlibrary_shared_apiexample.Sublayout_britishlibrary_shared_apiexampleSublayout" Codebehind="~/layouts/BritishLibrary/Shared/Sublayout_BritishLibrary_Shared_ApiExample.ascx.cs" %>
+<asp:TextBox ID="tbxInput" runat="server" />
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+
---
+++ Source/Website/BritishLibrary.Website/layouts/BritishLibrary/Shared/Sublayout_BritishLibrary_Shared_ApiExample.ascx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Layouts.Sublayout_britishlibrary_shared_apiexample {
+  
+	/// <summary>
+	/// Summary description for Sublayout_britishlibrary_shared_apiexampleSublayout
+	/// </summary>
+  public partial class Sublayout_britishlibrary_shared_apiexampleSublayout : System.Web.UI.UserControl 
+	{
+    private void Page_Load(object sender, EventArgs e) {
+      // Put user code to initialize the page here
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write(tbxInput.Text);
+    }
+  }
+}
